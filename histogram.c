54ca74b4df8e1cdb5006c21bd1ccbbd2fe80f715
Nedas Cerneckis <nedcerneckis@gmail.com>
2024-06-10 22:16:01 +0100

    need to finish histogram

---
+++ histogram.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+#define MAX_HISTOGRAM_SIZE 15
+
+int main(){
+  int c, nw, nc, wc, state;
+  int cum_freq[MAX_HISTOGRAM_SIZE];
+
+  state = OUT;
+  nw = nc = wc = 0;
+  while((c = getchar()) != EOF){
+    ++nc;
+    if (c == ' ' || c == '\n' || c == '\t'){
+      wc = 0;
+      state = OUT;  
+    } else if (state == OUT){
+      state = IN;
+      ++wc
+      ++nw;
+    }
+  }
+
+  
+}
